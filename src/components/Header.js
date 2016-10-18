7ca46dbfc85a818fd6a4a52b20dddc0210a6d292
Andrea <alingenfelter2003@gmail.com>
2016-10-18 15:44:14 -0400

    updated files

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const h = require('react-hyperscript')
+const React = require('react')
+
+const Header = React.createClass({
+  render: function () {
+    return h('header', [
+      h('h1.f3.white', this.props.title)
+    ])
+  }
+})
+
+module.exports = Header
---
+++ src/components/List.js
@@ -0,0 +1,10 @@
+const h = require('react-hyperscript')
+
+const ListItem = require('./ListItem')
+
+const List = props => h('section.list', [
+  h('ul', props.items.map(item =>
+    h(ListItem, { item, onScoreChange: props.onScoreChange })
+  ))
+])
+module.exports = List
